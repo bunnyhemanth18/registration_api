b4400e62f190b706ed4ef32aba5c6375b7b30876
bunnyhemanth18 <157891130+bunnyhemanth18@users.noreply.github.com>
2024-02-06 19:52:06 +0530

    settings.py

--- mydrfproject/mydrfproject/settings.py
+++ mydrfproject/mydrfproject/settings.py
@@ -133,6 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ],
-    # Other settings...
+    ]
 }
